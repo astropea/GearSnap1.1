6758eb3c1a43706e6b31c5bbd7b16d5a9d95540f
Antoine Salagnac <antoinesalagnac430@gmail.com>
2025-11-19 10:48:20 +0100

    map+aviss

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.navigation:navigation-compose:2.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
